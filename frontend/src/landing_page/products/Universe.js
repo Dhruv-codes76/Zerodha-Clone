4d25a321f840bb1005a39b356c52963aee79ab3f
Dhruv-codes76 <dhruvnvishwa@gmail.com>
2025-09-23 10:30:38 +0530

    Fix: added react-router-dom and frontend build success

--- frontend/src/landing_page/products/Universe.js
+++ frontend/src/landing_page/products/Universe.js
@@ -9,36 +9,55 @@ function Universe() {
           Extend your trading and investment experience even further with our
           partner platforms
         </p>
-
-        <div className="col-4 p-3 mt-5">
-          <img src="media/images/smallcaseLogo.png" />
-          <p className="text-small text-muted">Thematic investment platform</p>
+        <div className="col-4 mt-5">
+          <img src="media\images\zerodhaFundhouse.png" style={{height:"50px",width:"175px"}} className="mb-3"/>
+          <p className="text-muted text-center text-small">
+            Our asset management venture that is creating simple and transparent
+            index funds to help you save for your goals.
+          </p>
         </div>
-        <div className="col-4 p-3 mt-5">
-          <img src="media/images/smallcaseLogo.png" />
-          <p className="text-small text-muted">Thematic investment platform</p>
+        <div className="col-4 mt-5">
+          <img src="media\images\sensibullLogo.svg" style={{height:"95px",width:"200px"}} className="mb-3"/>
+          <p className="text-muted text-small">
+            Options trading platform that lets you create strategies, analyze
+            positions, and examine data points like open interest, FII/DII, and
+            more.
+          </p>
         </div>
-        <div className="col-4 p-3 mt-5">
-          <img src="media/images/smallcaseLogo.png" />
-          <p className="text-small text-muted">Thematic investment platform</p>
+        <div className="col-4 mt-5 ">
+          <img src="media\images\tijori.svg" style={{height:"50px",width:"150px"}}/>
+          <p className="text-muted text-small">
+            Investment research platform that offers detailed insights on
+            stocks, sectors, supply chains, and more.
+          </p>
         </div>
-        <div className="col-4 p-3 mt-5">
-          <img src="media/images/smallcaseLogo.png" />
-          <p className="text-small text-muted">Thematic investment platform</p>
+        <div className="col-4 mt-5">
+          <img src="media\images\streakLogo.png" style={{height:"55px",width:"160px"}} className="mb-3"/>
+          <p className="text-muted fs-6">
+            Systematic trading platform that allows you to create and backtest
+            strategies without coding.
+          </p>
         </div>
-        <div className="col-4 p-3 mt-5">
-          <img src="media/images/smallcaseLogo.png" />
-          <p className="text-small text-muted">Thematic investment platform</p>
+        <div className="col-4 mt-5">
+          <img src="media\images\smallcaseLogo.png" style={{height:"50px",width:"200px"}} className="mb-3"/>
+          <p className="text-muted text-small">
+            Thematic investing platform that helps you invest in diversified
+            baskets of stocks on ETFs.
+          </p>
         </div>
-        <div className="col-4 p-3 mt-5">
-          <img src="media/images/smallcaseLogo.png" />
-          <p className="text-small text-muted">Thematic investment platform</p>
+        
+        <div className="col-4 mt-5 ">
+          <img src="media\images\dittoLogo.png" style={{height:"50px",width:"130px"}} className="mb-3"/>
+          <p className="text-muted text-small">
+            Personalized advice on life and health insurance. No spam and no
+            mis-selling.
+          </p>
         </div>
         <button
-          className="p-2 btn btn-primary fs-5 mb-5"
+          className="p-2 btn btn-primary fs-5 mt-5 mb-5"
           style={{ width: "20%", margin: "0 auto" }}
         >
-          Signup Now
+          Sign up for free
         </button>
       </div>
     </div>
